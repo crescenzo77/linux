6fed50e52bde226b403e57140980fc374a0d8b81
Enzo Adriano <enzo.adriano.code@gmail.com>
2026-06-03 20:57:04 -0400

    dt-bindings: clock: add Allwinner A733 CCU
    Add a binding for the main clock control unit found in the
    Allwinner A733 SoC.
    
    Signed-off-by: Enzo Adriano <enzo.adriano.code@gmail.com>

---
+++ include/dt-bindings/reset/sun60i-a733-ccu.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR MIT) */
+
+#ifndef _DT_BINDINGS_RST_SUN60I_A733_CCU_H_
+#define _DT_BINDINGS_RST_SUN60I_A733_CCU_H_
+
+#define RST_BUS_MMC0		35
+#define RST_BUS_UART0		41
+
+#endif /* _DT_BINDINGS_RST_SUN60I_A733_CCU_H_ */
